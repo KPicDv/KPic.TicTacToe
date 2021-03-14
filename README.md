734c7b089903e4546822757bf09d3b29d7e43916
KPic <kevinpichon@outlook.fr>
2021-03-14 15:51:15 +0100

    Affichage du jeu

--- README.md
+++
@@ -1,1 +0,0 @@
-KPic.TicTacToc
---
+++ app.js
@@ -0,0 +1,166 @@
+let nomJoueurBleu = null
+let nomJoueurRouge = null
+let numeroTour = 1
+let gagnant = null
+let cases = [null, null, null, null, null, null, null, null, null]
+
+/**
+ * CASES
+ * 0 1 2
+ * 3 4 5
+ * 6 7 8
+ */
+
+// Récupération des zones HTML.
+const $boutonJouer = document.getElementById('jouer')
+const $boutonRecommencer = document.getElementById('recommencer')
+const $nomJoueurBleu = document.getElementById('nom-joueur-bleu')
+const $nomJoueurRouge = document.getElementById('nom-joueur-rouge')
+const $zoneSelectionJoueurs = document.getElementById('selection-joueurs')
+const $zoneJeu = document.getElementById('jeu')
+const $nomJoueur = document.getElementById('nom-joueur')
+const $tourJoueur = document.getElementById('tour-Joueur')
+const $tableau = document.getElementById('tableau')
+const $resultat = document.getElementById('resultat')
+const $casesTableau = document.querySelectorAll('#tableau td')
+const $page = document.querySelector('body')
+
+$boutonRecommencer.hidden = true
+$zoneJeu.hidden = true
+$resultat.hidden = true
+
+const tourSuivant = () => {
+    numeroTour++
+
+    if (numeroTour % 2 == 1) {
+        $nomJoueur.innerHTML = nomJoueurBleu
+        $nomJoueur.className = 'bleu-fonce'
+        $page.className = 'bleu'
+    } else {
+        $nomJoueur.innerHTML = nomJoueurRouge
+        $nomJoueur.className = 'rouge-fonce'
+        $page.className = 'rouge'
+    }
+}
+
+// Lorsque la partie est terminée.
+const partieTerminee = () => {
+    $resultat.innerHTML = 'Match nul !'
+    $page.className = ''
+    $tourJoueur.hidden = true
+    $resultat.hidden = false
+    $boutonRecommencer.hidden = false
+}
+
+// Vérification si un joueur a gagné.
+const verifierVictoire = () => {
+    // Lignes.
+    const victoireSurLigne = verifierCases(0, 1, 2) || verifierCases(3, 4, 5) || verifierCases(6, 7, 8)
+    
+    if (victoireSurLigne) {
+        return victoireSurLigne
+    } else {
+        // Colones.
+        const victoireSurColonne = verifierCases(0, 3, 6) || verifierCases(1, 4, 7) || verifierCases(2, 5, 8)
+        
+        if (victoireSurColonne) {
+            return victoireSurColonne
+        } else {
+            // Diagonales.
+            const victoireSurDiagonale = verifierCases(0, 4, 8) || verifierCases(2, 4, 6)
+
+            if (victoireSurDiagonale) {
+                return victoireSurDiagonale
+            }
+        }
+    }
+
+    return null
+}
+
+// Vérification si 3 cases ont le même signe.
+const verifierCases = (a, b, c) => {
+    const victoire = cases[a] != null && cases[a] == cases[b] && cases[a] == cases[c]
+
+    if (victoire) {
+        return cases[a]
+    }
+    return null
+}
+
+// Lorsqu'on clique sur le bouton JOUER.
+$boutonJouer.onclick = () => {
+    nomJoueurBleu = $nomJoueurBleu.value
+    nomJoueurRouge = $nomJoueurRouge.value
+
+    if (nomJoueurBleu.length < 3 || nomJoueurBleu.length < 3) {
+        alert('Les noms sont invalides')
+
+        nomJoueurBleu = null
+        nomJoueurBleu = null
+    } else {
+        $zoneSelectionJoueurs.hidden = true
+        $zoneJeu.hidden = false
+        $nomJoueur.innerHTML = nomJoueurBleu
+        $nomJoueur.className = 'bleu'
+        $page.className = 'bleu'
+    }
+}
+
+// Lorsqu'on clique sur le bouton RECOMMENCER.
+$boutonRecommencer.onclick = () => {
+    numeroTour = 1
+    gagnant = null
+    cases = [null, null, null, null, null, null, null, null, null]
+    $resultat.innerHTML = ''
+    $page.className = 'bleu'
+    $tourJoueur.hidden = false
+    $resultat.hidden = true
+    $boutonRecommencer.hidden = true
+    
+    $casesTableau.forEach(($caseTableau) => {
+        $caseTableau.innerHTML = ''
+    })
+}
+
+// Parcours toutes les cellules du tableau pour ajouter les évènements
+$casesTableau.forEach(($caseTableau, index) => {
+    // Lorsqu'on clique sur la cellule du tableau.
+    $caseTableau.onclick = () => {
+        if (cases[index] == null && gagnant == null) {
+            let nomImage
+            if (numeroTour % 2 == 1) {
+                nomImage = 'rond'
+                cases[index] = 1
+            } else {
+                nomImage = 'croix'
+                cases[index] = 2
+            }
+            $caseTableau.innerHTML = '<img src="images/' + nomImage + '.png">'
+
+            gagnant = verifierVictoire()
+
+            if (gagnant) {
+                let nomGagnant
+                let couleurGagnant
+                if (gagnant == 1) {
+                    nomGagnant = nomJoueurBleu
+                    couleurGagnant = 'bleu'
+                } else {
+                    nomGagnant = nomJoueurRouge
+                    couleurGagnant = 'rouge'
+                }
+                $resultat.innerHTML = 'Victoire de <span class="' + couleurGagnant + '">' + nomGagnant + '</span> !'
+                $tourJoueur.hidden = true
+                $resultat.hidden = false
+                $boutonRecommencer.hidden = false
+            } else {
+                if (numeroTour == 9) {
+                    partieTerminee()
+                } else {
+                    tourSuivant()
+                }
+            }
+        }
+    }
+})
